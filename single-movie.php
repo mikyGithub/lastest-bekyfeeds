4da2b36d284abf008d09d5cd63dab7ff2458f4d6
Hailemichael Adefrs <qechachaw@gmail.com>
2021-07-22 17:42:44 +0300

    update on index

--- single-movie.php
+++ single-movie.php
@@ -1,33 +1,8 @@
 <!DOCTYPE html>
 <html>
-<?php require "config/meta.php"; ?>
-
-<head>
-    <title>BekyFeeds</title>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.min.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/animate.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/font.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/li-scroller.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/slick.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/jquery.fancybox.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/theme.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/style.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/utilities.min.css" />
-    <?php require "config/js.php"; ?>
-    <!--[if lt IE 9]>
-      <script src="../assets/js/html5shiv.min.js"></script>
-      <script src="../assets/js/respond.min.js"></script>
-    <![endif]-->
-</head>
-<!-- PHP -->
-
 <?php
 
-
+require "config/meta.php"; 
 require 'models/FeatureFilm.php';
 require 'config/Database.php';
 $database = new Database();
@@ -91,6 +66,32 @@ if($isParameter === false){
 
 
 ?>
+
+
+
+<head>
+    <title><?php echo "index of ".$title; ?></title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/animate.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/font.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/li-scroller.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/slick.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/jquery.fancybox.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/theme.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/utilities.min.css" />
+    <?php require "config/js.php"; ?>
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.min.js"></script>
+      <script src="../assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<!-- PHP -->
+
 
 
 
--- single-series.php
+++ single-series.php
@@ -1,31 +1,6 @@
 <!DOCTYPE html>
 <html>
-<?php require "config/meta.php"; ?>
-
-<head>
-    <title>BekyFeeds</title>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.min.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/animate.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/font.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/li-scroller.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/slick.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/jquery.fancybox.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/theme.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/style.css" />
-    <link rel="stylesheet" type="text/css" href="../assets/css/utilities.min.css" /> <?php require "config/js.php"; ?>
-    <!--[if lt IE 9]>
-      <script src="../assets/js/html5shiv.min.js"></script>
-      <script src="../assets/js/respond.min.js"></script>
-    <![endif]-->
-</head>
-<!-- PHP -->
-
-<?php
-
+<?php require "config/meta.php"; 
 
 require 'models/Series.php';
 require 'config/Database.php';
@@ -87,7 +62,34 @@ if($isParameter === false){
 }
 
 
+
 ?>
+
+<head>
+    <title><?php echo "index of ".$name; ?></title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/animate.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/font.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/li-scroller.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/slick.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/jquery.fancybox.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/theme.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/css/utilities.min.css" /> <?php require "config/js.php"; ?>
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.min.js"></script>
+      <script src="../assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<!-- PHP -->
+
+
+
+
 
 
 
